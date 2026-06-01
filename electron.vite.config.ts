286936e12ea86a2c48de90e9f66c40f0dc05096d
Paolo <186249117+PaoloESAN@users.noreply.github.com>
2026-06-01 13:54:30 -0500

    refactor: $lib alias en ts

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'electron-vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
   main: {},
   preload: {},
   renderer: {
-    plugins: [tailwindcss() as any, svelte() as any]
+    plugins: [tailwindcss() as any, svelte() as any],
+    resolve: {
+      alias: {
+        $lib: path.resolve(__dirname, 'src/renderer/src/lib'),
+      }
+    }
   }
 })
